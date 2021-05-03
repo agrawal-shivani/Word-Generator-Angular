0b036c05b1c14b558e2861fb9017c3b7cc903779
agrawal-shivani <shivani.agrawal@sugaldamani.com>
2021-05-03 13:52:07 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utility/words' ;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+  words = '';
+  limit = 10;
+  handleSliderChange(newLimit: number): void {
+    this.limit = newLimit;
+  }
+  generator(): void {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
 }
